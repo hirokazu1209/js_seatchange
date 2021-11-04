c1bcb12fd181a8607633a5d74df856829d9fe940
hirokazu <n.hirokazu92@gmail.com>
2021-11-04 09:48:35 +0900

    shuffle実装

---
+++ techpit-javascript-seat-startdocs-master/assets/js/script.js
@@ -0,0 +1,28 @@
+const studentNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+
+const shuffleArray = function(){
+  for(let i = studentNumbers.length - 1;i > 0; i--){
+    const randomNum = Math.floor(Math.random() * i);
+    let tmp = studentNumbers[i-1];
+    studentNumbers[i-1] = studentNumbers[randomNum];
+    studentNumbers[randomNum] = tmp;
+  }
+}
+
+const showSeatBoxes = function(){
+  let insertHTML = '';
+    studentNumbers.forEach(function(num){
+    insertHTML += '<div class="seat__item">' + num + '</div>';
+  })
+
+  document.querySelector('#seat').innerHTML = insertHTML;
+}
+
+shuffleArray();
+showSeatBoxes();
+
+// = は、JavaScriptにおいて = の右側にあるものを左側へ代入・保存・設定するという意味
+// forは繰り返し処理を行う回数を具体的にfor文に教える必要がある
+// Math.ceil()は()の中に指定した数値データの小数点以下部分を無条件で切り上げすることができる
+// Math.floor()は小数点以下を切り捨てする命令
+// 関数とは「複数の処理・命令を1つにまとめたもの」
