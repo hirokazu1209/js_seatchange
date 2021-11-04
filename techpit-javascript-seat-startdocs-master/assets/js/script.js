c65f462e39173304a41c32254cb023300c9af0c6
hirokazu <n.hirokazu92@gmail.com>
2021-11-05 01:07:37 +0900

    drum-sound実装

--- techpit-javascript-seat-startdocs-master/assets/js/script.js
+++ techpit-javascript-seat-startdocs-master/assets/js/script.js
@@ -18,11 +18,29 @@ const showSeatBoxes = function(){
   document.querySelector('#seat').innerHTML = insertHTML;
 }
 
-shuffleArray();
-showSeatBoxes();
+const soundPlay = function(){
+  const audioElement = new Audio();
+  audioElement.src = 'assets/audio/drum.mp3';
+  audioElement.play();
+
+  audioElement.addEventListener('ended', function(){
+    clearInterval(timer);
+  })
+}
+
+document.querySelector('#btn-start').addEventListener('click', function(){
+  timer = setInterval(function(){
+    shuffleArray();
+    showSeatBoxes();
+  }, 50);
+  soundPlay();
+});
+
+
 
 // = は、JavaScriptにおいて = の右側にあるものを左側へ代入・保存・設定するという意味
 // forは繰り返し処理を行う回数を具体的にfor文に教える必要がある
 // Math.ceil()は()の中に指定した数値データの小数点以下部分を無条件で切り上げすることができる
 // Math.floor()は小数点以下を切り捨てする命令
 // 関数とは「複数の処理・命令を1つにまとめたもの」
+// setInterval()の基本コード setInterval('繰り返す命令の内容','タイマー処理を行う時間の間隔')
